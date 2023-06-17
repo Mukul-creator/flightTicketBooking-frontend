baf07f13e436cfe68c60f86c2f72d9912cb04162
Mukul-creator <mukulkumar2652@gmail.com>
2023-06-17 21:07:10 +0530

    bux-fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 
 // import Layout from "./components/layout/Layout";
-import IndexPage from "./pages/Index";
+// import IndexPage from "./pages/Index";
 
 import MainNavigation from "./components/layout/MainNavigation";
 import RegisterPage from "./pages/Register";
